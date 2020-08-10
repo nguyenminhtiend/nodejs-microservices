67c06bc54e2c944eed0e3bd3b91910b33bc77d4e
Tien Nguyen <nguyenminhtiend@gmail.com>
2020-08-10 15:47:48 +0700

    #

--- tests/setup.js
+++ tests/setup.js
@@ -1,12 +1,12 @@
-const path = require('path');
+// const path = require('path');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const chaiLike = require('chai-like');
-require('dotenv').config({
-  path: path.resolve(process.cwd(), '.env.test'),
-});
+// require('dotenv').config({
+//   path: path.resolve(process.cwd(), '.env.test'),
+// });
 
-const startApp = require('../src/shared/app');
+// const startApp = require('../src/shared/app');
 
 global.chai = chai;
 global.expect = chai.expect;
@@ -14,6 +14,6 @@ global.expect = chai.expect;
 chai.use(chaiHttp);
 chai.use(chaiLike);
 
-const serviceName = process.argv[4];
-const app = startApp(serviceName.replace('--', ''));
-global.app = app;
+// const serviceName = process.argv[4];
+// const app = startApp(serviceName.replace('--', ''));
+// global.app = app;
---
+++ tests/user/employee/sample.spec.js
@@ -0,0 +1,5 @@
+describe('## Sample test', () => {
+  it('should return true', async () => {
+    expect(true).to.equal(true);
+  });
+});
