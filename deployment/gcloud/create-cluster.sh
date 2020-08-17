0c74843ac4bc2430c076b2dc51b9aee78b121629
Tien Nguyen <nguyenminhtiend@gmail.com>
2020-08-17 15:45:08 +0700

    #

--- deployment/gcloud/create-cluster.sh
+++
@@ -1,7 +0,0 @@
-# gcloud container clusters create messi-cluster \
-#       --num-nodes=1 \
-#       --disk-type=pd-standard \
-#       --disk-size=10GB \
-gcloud container clusters create messi-cluster
-#gcloud container clusters resize my-cluster --num-nodes=1
-#gcloud container clusters get-credentials messi-cluster
