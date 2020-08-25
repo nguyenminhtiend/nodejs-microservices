f3ff70bd4030e0090372e43fb3c953e8528e3952
Tien Nguyen <nguyenminhtiend@gmail.com>
2020-08-25 22:06:15 +0700

    #

--- src/department-service/config/sequelize.js
+++ src/department-service/config/sequelize.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  config: path.resolve('src', 'user', 'config', 'index.js'),
-  'migrations-path': path.resolve('src', 'user', 'migrations'),
-  'models-path': path.resolve('src', 'user', 'models'),
+  config: path.resolve('src', 'department-service', 'config', 'index.js'),
+  'migrations-path': path.resolve('src', 'department-service', 'migrations'),
+  'models-path': path.resolve('src', 'department-service', 'models'),
 };
